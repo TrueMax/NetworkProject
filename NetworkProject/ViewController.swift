a1dff082de8a7ac8c3b677a6e8105542e02ccce3
Maxim Abakumov <maximnatural@gmail.com>
2021-03-10 19:45:10 +0300

    Initial Commit

---
+++ NetworkProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+// ViewController.swift
+// NetworkProject
+// 
+// Created by Maxim Abakumov on 2021. 03. 10.
+//
+// Copyright © 2020, Maxim Abakumov. MIT License.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
